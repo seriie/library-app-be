daf6fcaf2650d1922c4c4ea2fbd20eaa16c79a4e
zee <mohammadzidane058@gmail.com>
2026-02-11 09:45:28 +0700

    fix: user tracking

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -12,40 +12,6 @@ export const getSessions = async (req, res) => {
   }
 }
 
-export const createSession = async (req, res) => {
-  try {
-    const { userId } = req.body;
-
-    if (!userId) {
-      return res
-        .status(400)
-        .json({ message: "userId is required", code: 400 });
-    }
-
-    const expiredAt = new Date(Date.now() + 60 * 60 * 1000);
-
-    await prisma.session.deleteMany({
-      where: { userId },
-    });
-
-    const session = await prisma.session.create({
-      data: {
-        id: nanoIdFormat("suid-"),
-        userId,
-        expiredAt,
-      },
-    });
-
-    res.status(201).json({
-      message: "Session created",
-      code: 201,
-      data: session,
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message, code: 500 });
-  }
-};
-
 export const deleteSession = async (req, res) => {
   const { id } = req.params;
 
--- src/routes/session.route.js
+++ src/routes/session.route.js
@@ -1,10 +1,8 @@
 import express from "express";
 const router = express.Router();
 import * as sessionController from "../controllers/session.controller.js";
-import verifyToken from '../middlewares/auth.middleware.js';
 
-router.post("/", verifyToken, sessionController.createSession);
-router.get("/", verifyToken, sessionController.getSessions);
-router.delete("/:id", verifyToken, sessionController.deleteSession);
+router.get("/", sessionController.getSessions);
+router.delete("/:id", sessionController.deleteSession);
 
 export default router;
